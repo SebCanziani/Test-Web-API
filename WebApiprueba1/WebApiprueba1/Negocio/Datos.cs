b3215ffc431279588569af16fff88c44b2b39ecc
SebCanziani <sebicanziani@gmail.com>
2024-09-24 20:09:06 -0300

    --

---
+++ WebApiprueba1/WebApiprueba1/Negocio/Datos.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    internal class Datos
+    {
+        public static List<Producto> List = new List<Producto>()
+        {
+            new Producto() { Id = 1, Description = "verde", Name = "marcos" }
+
+        };
+
+    }
+
+}
---
+++ WebApiprueba1/WebApiprueba1/WebApiprueba1/ValuesController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Negocio;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApiprueba1
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public List<Producto> Get()
+
+            
+        {
+
+            ProductosApi productosApi = new ProductosApi();
+            var products = productosApi.GetAll();
+
+            return products;
+
+            /* La opción simplificada
+             
+                ProductosApi productosApi = new ProductosApi();
+                return productosApi.GetAll();
+
+             */
+        }
+
+        // GET api/<ValuesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ValuesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ValuesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ValuesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
